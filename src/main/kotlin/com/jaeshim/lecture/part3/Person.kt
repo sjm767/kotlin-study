53bedd324a561eb6b9524b5c9b0c30f48cfd3779
jaeshim <jaeshim@gmarket.com>
2022-11-16 15:58:04 +0900

    lec09

--- src/main/kotlin/com/jaeshim/lecture/part3/Person.kt
+++ src/main/kotlin/com/jaeshim/lecture/part3/Person.kt
@@ -4,8 +4,23 @@ import java.lang.IllegalArgumentException
 
 
 class Person (
-    val name: String="jaeshim",
+    name: String="jaeshim",
     var age: Int=1){
+
+//    fun getUppercaseName(): String{
+//        return this.name.uppercase()
+//    }
+
+//    val uppercaseName: String
+//        get() = this.name.uppercase()
+
+
+    //setter
+    var name = name
+        set(value){
+            field = value.uppercase()
+        }
+
 
     //클래스가 초기화될 때 한 번 실행되는 블록
     init{
@@ -21,4 +36,15 @@ class Person (
     constructor() : this("jaeshim"){
         println("두번째 부생성자")
     }
+
+//    fun isAdult(): Boolean{
+//        return this.age>=20
+//    }
+
+//    val isAdult: Boolean
+//        get() = this.age >= 20
+      val isAdult: Boolean
+            get() {
+                return this.age >= 20
+            }
 }
