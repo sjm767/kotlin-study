98d1554e6f2b1dfe4fe85d437b76dd67a26b9516
jaeshim <jaeshim@gmarket.com>
2022-11-25 08:55:04 +0900

    lec16

--- src/main/kotlin/com/jaeshim/lecture/part4/lec15.kt
+++ src/main/kotlin/com/jaeshim/lecture/part4/lec15.kt
@@ -8,11 +8,59 @@ fun main(){
 //        println("${i}, ${array[i]}")
 //    }
 
-    //index와 value를 한 번에 받는 방법
-    for((idx,value) in array.withIndex()){
-        println("${idx},${value}")
+//    //index와 value를 한 번에 받는 방법
+//    for((idx,value) in array.withIndex()){
+//        println("${idx},${value}")
+//    }
+//
+//    val numbers = listOf(100,200)
+//    //비어있는 리스트. 들어올 타입이 무엇인지 명시해줘야 한다.
+//    printNumbers(emptyList())
+
+//
+//    val numbers = listOf(100,200)
+//
+//    //가능은 하다
+//    numbers.get(0)
+//
+//    //배열처럼 가져오기
+//    println(numbers[0])
+//
+//    //for-each문 사용
+//    for(number in numbers){
+//        println(number)
+//    }
+//    for((idx,value) in numbers.withIndex()){
+//        println("${idx} ${value}")
+//    }
+
+//    val numbers = mutableListOf(100,200)
+//    numbers.add(300)
+
+//    val numbers = setOf(100,200);
+//
+//    for(number in numbers){
+//        println(number)
+//    }
+//
+//    for((idx,value) in numbers.withIndex()){
+//        println("${idx} ${value}")
+//    }
+
+    val oldMap = mutableMapOf<Int,String>()
+    oldMap[1] = "MONDAY"
+    oldMap[2] = "TUESDAY"
+
+    for(key in oldMap.keys){
+        println(oldMap[key])
     }
 
+    for((key,value) in oldMap.entries){
+        println(key)
+        println(value)
+    }
+}
 
+private fun printNumbers(numbers: List<Int>){
 
 }
--- src/main/kotlin/com/jaeshim/lecture/part4/lec16.kt
+++ src/main/kotlin/com/jaeshim/lecture/part4/lec16.kt
@@ -1,4 +1,46 @@
 package com.jaeshim.lecture.part4
 
-class lec16 {
+fun main(){
+// val str ="ABC"
+//    println( str.lastChar())
+
+    3.add3(4)
 }
+
+fun String.lastChar(): Char{
+    return this[this.length-1]
+}
+
+val String.lastChar: Char
+    get() = this[this.length-1]
+
+
+open class Train(
+    val name: String = "새마을",
+    val price: Int = 5000,
+)
+
+fun Train.isExpensive(): Boolean {
+    println("Train의 확장함수")
+    return this.price >= 10000
+}
+
+class Srt : Train("SRT",40000)
+
+fun Srt.isExpensive(): Boolean {
+    println("Srt의 확장함수")
+
+    return this.price >= 10000
+
+}
+
+fun Int.add(other: Int): Int{
+    return this+other
+}
+infix fun Int.add2(other: Int): Int{
+    return this+other
+}
+inline fun Int.add3(other: Int): Int{
+    return this+other
+}
+
